1db7ce253ef1894eb2100154c06726c8aa908393
Lucas <119642350+lucascrlsn@users.noreply.github.com>
2023-01-06 17:49:09 -0600

    Create multiLineRegex.js

---
+++ tools/multiLineRegex.js
@@ -0,0 +1,28 @@
+function testREGEX() {
+
+  try {
+
+  let rawSheetData = `This is a test of a multi-
+  line string. There should be
+  at least two lines in this string that should be 
+  split, removed, combined, and re-written into a single
+  line string.`;
+
+  //let sheetData = Utilities.parseCsv(rawSheetData);
+  let unformattedSheetData = Utilities.parseCsv(rawSheetData);
+
+  //let regexExp = new RegExp("(/(\r\n|\n|\r)/)","gm");
+  let regexExp = new RegExp("(/\n|\r/g)","");
+
+  // CONVERT TO STRING, SPLIT BY REGEX PARAMS, EXECUTE REGEX, REJOIN 
+  let sheetData = unformattedSheetData.toString().split(regexExp).join();
+
+  console.log(sheetData);
+  
+
+  } catch (err) {
+    // HANDLE EXCEPTION
+    console.log('Failed with error: %s', err.message);
+  }
+
+}
