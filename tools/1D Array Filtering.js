4749b2bc7f2667fb8ba11503a2f5a3c1d5a710e2
Lucas <119642350+lucascrlsn@users.noreply.github.com>
2023-01-06 22:55:39 -0600

    Create 1D Array Filtering.js

---
+++ tools/1D Array Filtering.js
@@ -0,0 +1,39 @@
+function 1DArrayFilter() {
+
+  try {
+
+    let sampleArray = [10,"text3563",6,3,77,"ejetextkj25"];
+
+    let newArray = sampleArray.filter(filterLogic)
+
+    console.log(newArray);
+
+  } catch (err) {
+    // HANDLE EXCEPTION
+    console.log('Failed with error: %s', err.message);
+  }
+
+}
+
+
+let filterLogic = function(item){
+
+  try {
+
+    if(item.toString().indexOf("text") === -1){
+      return true;
+    } else {
+      return false;
+    }
+
+    } catch (err) {
+      // HANDLE EXCEPTION
+      console.log('Failed with error: %s', err.message);
+    }
+
+}
+
+
+
+
+
