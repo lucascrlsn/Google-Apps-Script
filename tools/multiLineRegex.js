c7d86bad80d2580c863c8617f1f0a281335fa939
Lucas <119642350+lucascrlsn@users.noreply.github.com>
2023-01-06 22:25:12 -0600

    Minor Formatting

--- tools/multiLineRegex.js
+++ tools/multiLineRegex.js
@@ -2,23 +2,22 @@ function testREGEX() {
 
   try {
 
-  let rawSheetData = `This is a test of a multi-
-  line string. There should be
-  at least two lines in this string that should be 
-  split, removed, combined, and re-written into a single
-  line string.`;
+    let rawSheetData = `This is a test of a multi-
+    line string. There should be
+    at least two lines in this string that should be 
+    split, removed, combined, and re-written into a single
+    line string.`;
 
-  //let sheetData = Utilities.parseCsv(rawSheetData);
-  let unformattedSheetData = Utilities.parseCsv(rawSheetData);
+    //let sheetData = Utilities.parseCsv(rawSheetData);
+    let unformattedSheetData = Utilities.parseCsv(rawSheetData);
 
-  //let regexExp = new RegExp("(/(\r\n|\n|\r)/)","gm");
-  let regexExp = new RegExp("(/\n|\r/g)","");
+    //let regexExp = new RegExp("(/(\r\n|\n|\r)/)","gm");
+    let regexExp = new RegExp("(/\n|\r/g)","");
 
-  // CONVERT TO STRING, SPLIT BY REGEX PARAMS, EXECUTE REGEX, REJOIN 
-  let sheetData = unformattedSheetData.toString().split(regexExp).join();
+    // CONVERT TO STRING, SPLIT BY REGEX PARAMS, EXECUTE REGEX, REJOIN 
+    let sheetData = unformattedSheetData.toString().split(regexExp).join();
 
-  console.log(sheetData);
-  
+    console.log(sheetData);
 
   } catch (err) {
     // HANDLE EXCEPTION
